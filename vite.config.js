7762985ca05b3b73645d5d46a934fb2b09249c48
relbns <ariel@stoketalent.com>
2025-02-26 03:00:03 +0200

    .

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,15 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Base path for GitHub Pages - replace 'gist-storage-app' with your repo name
-  // base: '/gh-pages-gist/',
   base: './', // This ensures assets are loaded correctly on GitHub Pages
-  css: {
-    postcss: './postcss.config.js',
-  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    emptyOutDir: true,
+    sourcemap: false,
+  }
 })
