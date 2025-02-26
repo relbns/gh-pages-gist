49559cf955bf86dbb019b0430c9177d31a81a5ff
relbns <ariel@stoketalent.com>
2025-02-26 09:43:51 +0200

    .

--- vite.config.js
+++ vite.config.js
@@ -4,24 +4,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/gh-pages-gist/',
-  build: {
-    outDir: 'dist',
-    rollupOptions: {
-      output: {
-        // Ensure proper file extensions and formats
-        entryFileNames: 'assets/[name]-[hash].js',
-        chunkFileNames: 'assets/[name]-[hash].js',
-        assetFileNames: 'assets/[name]-[hash].[ext]'
-      }
-    },
-    // Make sure sourcemaps are disabled in production
-    sourcemap: false,
-  },
-  // Ensure the proper MIME types for JSX files
-  server: {
-    headers: {
-      'Content-Type': 'application/javascript',
-    },
-  },
+  base: '/gh-pages-gist'
 })
